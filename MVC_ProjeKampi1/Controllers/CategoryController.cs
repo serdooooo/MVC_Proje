73dfc885f66b6fb3a4edbf8589abcbc08c709711
karakurtserdar19@gmail.com <karakurtserdar19@gmail.com>
2022-01-25 19:11:07 +0300

    revised

--- MVC_ProjeKampi1/Controllers/CategoryController.cs
+++ MVC_ProjeKampi1/Controllers/CategoryController.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using BusinessLayer.Concrete;
+using DataAccessLayer.EntityFramework;
+using EntityLayer.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +12,26 @@ namespace MVC_ProjeKampi1.Controllers
     public class CategoryController : Controller
     {
         // GET: Category
+        CategoryManager cm = new CategoryManager(new EFCategoryDal());
         public ActionResult Index()
         {
             return View();
+        }
+        public ActionResult GetCategoryList()
+        {
+            var categoryValues = cm.GetList();
+            return View(categoryValues);
+        }
+        [HttpGet] //sayfa yükledii zaman http get devreye girecek
+        public ActionResult AddCategory()
+        {
+            return View();
+        }
+        [HttpPost] //eer bir butona tıklarsam post devreye girecek
+        public ActionResult AddCategory(Category p)
+        {
+            //cm.CategoryAddBL(p);
+            return RedirectToAction("GetCategoryList");
         }
     }
 }
